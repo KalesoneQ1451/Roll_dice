599a8db1de2ce61ab7eef68c3ab86933ab5ef07d
KalesoneQ1451 <kacper.jatczak2006@gmail.com>
2024-10-17 14:24:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/roll_dice/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.roll_dice;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView gra;
+    private Button losowanie;
+    private TextView kosc1;
+    private TextView kosc2;
+    private TextView kosc3;
+    private TextView kosc4;
+    private TextView kosc5;
+    private TextView wynikLosu;
+    private TextView wynikGry;
+    private Button reset;
+    private TextView iloscRzutow;
+    private int liczbaRzutow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        gra = findViewById(R.id.textView);
+        losowanie = findViewById(R.id.losowanie);
+        kosc1 = findViewById(R.id.kosc1);
+        kosc2 = findViewById(R.id.kosc2);
+        kosc3 = findViewById(R.id.kosc3);
+        kosc4 = findViewById(R.id.kosc4);
+        kosc5 = findViewById(R.id.kosc5);
+        wynikLosu = findViewById(R.id.wynikLos);
+        wynikGry = findViewById(R.id.wynikGry);
+        iloscRzutow = findViewById(R.id.ileLos);
+        reset = findViewById(R.id.reset);
+
+
+
+
+        losowanie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rollDice();
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetGame();
+            }
+        });
+    }
+    public void rollDice(){
+        Random rand = new Random();
+        int[] rzut = new int[5];
+        for (int i = 0; i < 5; i++) {
+            int liczba = rand.nextInt(6)+1;
+            rzut[i] = liczba;
+
+        }
+    }
+    public void resetGame(){
+        kosc1.setText("?");
+        kosc2.setText("?");
+        kosc3.setText("?");
+        kosc4.setText("?");
+        kosc5.setText("?");
+    }
+}
